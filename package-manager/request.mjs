8be1192ea2f0ff5dad63e4bbab6cf8d22d911895
abdulbasit <imabk@gmail.com>
2022-07-19 18:01:11 +0530

    Install nodemon

--- package-manager/request.mjs
+++ package-manager/request.mjs
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios
-  .get("http://www.google.com")
+  .get("http://www.google.org")
   .then((resp) => console.log("response", resp))
   .catch((err) => console.log(err));
