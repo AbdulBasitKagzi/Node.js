0ac3e48e8c6fc609941c72d6b7c3a1ff9a99ec53
abdulbasit <imabk@gmail.com>
2022-07-19 19:07:22 +0530

    Parse csv file

---
+++ planets-project/index.js
@@ -0,0 +1,8 @@
+const parse = require("csv-parse");
+const fs = require("fs");
+
+const result = [];
+fs.createReadStream("kepler_data.csv")
+  .on("data", (data) => result.push(data))
+  .on("error", (err) => console.log(err))
+  .on("end", () => console.log(result));
