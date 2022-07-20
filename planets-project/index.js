f549f8d6df1f6dec69b255a638dd2e8b6261452a
abdulbasit <imabk@gmail.com>
2022-07-20 12:37:27 +0530

    Project completed

--- planets-project/index.js
+++ planets-project/index.js
@@ -1,14 +1,33 @@
-const parse = require("csv-parse");
+const { parse } = require("csv-parse");
 const fs = require("fs");
 
 const result = [];
+
+function ishabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
 fs.createReadStream("kepler_data.csv")
   .pipe(
     parse({
       comment: "#",
-      columns: "true",
+      columns: true,
     })
   )
-  .on("data", (data) => result.push(data))
-  .on("error", (err) => console.log(err))
-  .on("end", () => console.log(result));
+  .on("data", (data) => {
+    if (ishabitablePlanet(data)) {
+      result.push(data);
+    }
+  })
+  .on("error", (err) => console.log("error", err))
+  .on("end", () => {
+    result.map((planet) => {
+      return console.log(planet["kepler_name"]);
+    });
+    console.log("Total planets", result.length);
+  });
