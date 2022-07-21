d5a047ed231face0137ddbb8be6d8438a2edac23
abdulbasit <imabk@gmail.com>
2022-07-21 11:51:12 +0530

    Use of mvc is done

---
+++ express-project/controllers/message.controller.js
@@ -0,0 +1,7 @@
+function getMessage(req, res) {
+  res.send(`There is a message for you`);
+}
+
+module.exports = {
+  getMessage,
+};
---
+++ express-project/models/friends.model.js
@@ -0,0 +1,19 @@
+const friends = [
+  {
+    id: 0,
+    name: "abdulbasit",
+  },
+  {
+    id: 1,
+    name: "rehan",
+  },
+
+  {
+    id: 2,
+    name: "huzifa",
+  },
+];
+
+module.exports = {
+  friends,
+};
